ac2e270eed323468cae547eab78e084e13a7700a
Fer <fernanbenitez@hotmail.com.ar>
2022-05-14 16:26:36 -0300

    FrontEnd
    Creación de la carpeta FrontEnd

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { LogoApComponent } from './componentes/logo-ap/logo-ap.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { from } from 'rxjs';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    RedesComponent,
+    LogoApComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent,
+    EducacionComponent,
+    BannerComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
