8f378a0d1a046cb38c219410a56a93050ba309b5
Fer <fernanbenitez@hotmail.com.ar>
2022-08-02 02:17:45 -0300

    Comps exp y edu funcionando

--- FrontEnd/src/app/service/seducacion.service.ts
+++ FrontEnd/src/app/service/seducacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class SEducacionService {
-  eduURL = 'http://localhost:8080/educacion/'
+  eduURL = 'http://localhost:8080/edu/'
 
   constructor(private httpClient: HttpClient) { }
 
